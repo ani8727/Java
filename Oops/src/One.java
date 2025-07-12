3ac3a6f1104cf557d12f41bde49b3a3110af8e46
ani8727 <aniketgupta.8727@gmail.com>
2025-07-12 18:07:02 +0530

    Oops

---
+++ Oops/src/One.java
@@ -0,0 +1,18 @@
+class Calculator{
+
+    public int add(int a, int b){
+        System.out.println("Done Calculation");
+        return a+b;
+    }
+}
+
+
+public class One {
+    public static void main(String[] args) {
+
+        Calculator c= new Calculator();
+        int result= c.add(5,8);
+        System.out.println(result);
+    }
+
+}
