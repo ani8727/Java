f7ffa52abc6355fc0506b97dd2c2f99c31c9e63d
ani8727 <aniketgupta.8727@gmail.com>
2025-07-13 12:14:52 +0530

    String Done

--- Intermediate/src/String1.java
+++ Intermediate/src/String1.java
@@ -1,2 +1,37 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.lang.foreign.SymbolLookup;
+import java.nio.channels.UnsupportedAddressTypeException;
+import java.util.stream.StreamSupport;
+
 public class String1 {
+    public static void main(String[] args) {
+//        Char Array -> String
+        char[] charArray = {'A','B','C'};
+        System.out.println(charArray);
+
+        String str = "Aniket";
+//        System.out.println(str);
+//        System.out.println(str.charAt(0));
+
+//        Ways to create String
+        String str1 = "Ani";
+        String str2 = "Ani";
+        String str3 = new String("Ani");
+        System.out.println(str1==str2);
+//        System.out.println(str1==str3);
+//        == operator used on instance not actual value
+        System.out.println(str1.equals(str3));
+
+        /*
+        String is immutable, so used all other methods, we used
+         stringBuffer and StringBuilder
+*/
+        StringBuffer str4 = new StringBuffer(("Ani"));
+        System.out.println(str4);
+        str4.append("ket");
+        System.out.println(str4);
+
+
+
+    }
 }
